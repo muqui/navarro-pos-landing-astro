46e065552fd6a2d37fc6b71a93aecc4b4bd5b786
Alberto Corona <muqui@hotmail.com>
2026-01-04 23:28:18 -0600

    cambios

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,16 @@
 import { defineConfig } from 'astro/config';
 import cloudflare from "@astrojs/cloudflare";
 
-import tailwindcss from '@tailwindcss/vite';
+
+import tailwindcss from "@tailwindcss/vite";
+
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  },
-   output: "static",
+  output: "static",
   adapter: cloudflare(),
-});
 
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
